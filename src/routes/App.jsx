e6b2486bcf183df5cb31287c5f0391c9d725a27a
Hector Valenzuela <francisco1495@gmail.com>
2023-03-31 23:21:13 -0700

    Agregamos mas archivos asi como terminamos un reto

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../containers/Layout'
-import Login from '../containers/Login';
+import CreateNewPassword from '../containers/CreateNewPassword';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import Login from '../containers/Login';
+import CreateNewAccount from '../containers/CreateNewAccount';
+import MyAccountEdit from '../containers/MyAccountEdit';
+import MyOrdAccMenu from '../components/MyOrdAccMenu';
+import MobileMenu from '../components/MobileMenu';
+import MyOrder from '../components/MyOrder';
+import MyOrders from '../components/MyOrders';
+import ProductDetail from '../components/ProductDetail';
+import CheckoutOrder from '../components/CheckoutOrder';
 import "../styles/Global.scss";
+
+
 
 
 const App = () => {
@@ -15,7 +26,16 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/create-new-password" element={<CreateNewPassword/>}/>
                 <Route path="/recovery-password" element={<RecoveryPassword/>}/>
+                <Route path="/create-new-account" element={<CreateNewAccount/>}/>
+                <Route path="/my-account-edit" element={<MyAccountEdit/>}/>
+                <Route path="/my-ord-acc-menu" element={<MyOrdAccMenu/>}/>
+                <Route path="/mobile-menu" element={<MobileMenu/>}/>
+                <Route path="/my-order" element={<MyOrder/>}/>
+                <Route path="/my-orders" element={<MyOrders/>}/>
+                <Route path="/product-detail" element={<ProductDetail/>}/>
+                <Route path="/checkout-order" element={<CheckoutOrder/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
         </Layout>
